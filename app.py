ee762b0d98310b7005749734656407e0da0a8c49
Ian Kahkonen <ikahkonen@PTM.local>
2020-07-21 11:14:32 -0400

    Added check for second page and a try counter for better error handling

--- app.py
+++ app.py
@@ -18,8 +18,10 @@ with open("ASIN.txt", "r") as text_file:
     for asin in text_file:
         print(asin.rstrip())
         included = False
+
         primecounter = 0 
         namelist = []
+
         with open('PrimeSellers.csv', 'a', newline='') as createFile:
             pass
         with open('PrimeSellers.csv', 'r', newline='', encoding='utf-8') as asinfile:
@@ -30,56 +32,21 @@ with open("ASIN.txt", "r") as text_file:
                     break
         if included == False:
             Error_status = False
-            try:
-                url = chrome.open(f"https://www.amazon.com/gp/offer-listing/{asin.rstrip()}")
-                soup = BeautifulSoup(url, 'html.parser')
-                with open("Output.txt", "w", encoding='utf-8') as text_file:
-                    text_file.write(str(soup))
+            try_counter = 0
+            success_status = False
+            nextpage = False
 
-                with open("Output.txt", "r", encoding='utf-8') as text_file:
-                    textParse = BeautifulSoup(text_file, 'html.parser')
+            while try_counter < 2 and success_status == False: 
+                if nextpage == True:
+                    adjusted_nextbutton = nextbutton.replace('next/', 'next?')
+                    url_append = str(adjusted_nextbutton)
+                else:
+                    url_append =  "gp/offer-listing/" + str(asin.rstrip())
 
-                    with open('PrimeSellers.csv', 'a+', newline='', encoding='utf-8') as csvfile:
-                        csvWriter = csv.writer(csvfile)
-                            
-                        if csvfile.tell() == 0:
-                            csvWriter.writerow(['Product ASIN', 'Seller Name', 'Prime Status', 'Condition'])
-
-                        sName = ''
-                        pStatus = ''
-                        qCondition = ''
-
-                        for i in textParse.findAll("div", {'class': 'a-row a-spacing-mini olpOffer'}):
-                            name = i.find("h3", {'class': 'a-spacing-none olpSellerName'})
-                            qCondition = i.find("div", {'class':'a-section a-spacing-small'}).get_text().strip()
-                            if "img" in str(name):
-                                 # print("Amazon")
-                                sName = "Amazon"
-                            else:
-                                # print(name.get_text().strip())
-                                sName = name.get_text().strip()
-
-                            if i.find("i", {'class': 'a-icon a-icon-prime'}):
-                                # print("Prime")
-                                pStatus = "Prime"
-                            else:
-                                # print("Not Prime")
-                                pStatus = "Not Prime"
-                            if i.find("i", {'class': 'a-icon a-icon-prime'}) and qCondition == 'New' and name.get_text().strip() not in namelist and name.get_text().strip() != 'paneltown':
-                                primecounter += 1
-                                namelist.append(name.get_text().strip())
-
-                            #print(f'ASIN: {asin.rstrip()}, Seller: {sName}, Prime Status: {pStatus}, Condition: {qCondition}')
-                                
-                            csvWriter.writerow([asin.rstrip(), sName, pStatus, qCondition])
-
-                                # print('\n')
-            #if there is an error with mechanize then try it all again 
-            except:
-                time.sleep(2)
                 try:
-                    url = chrome.open(f"https://www.amazon.com/gp/offer-listing/{asin.rstrip()}")
+                    url = chrome.open(f"https://www.amazon.com/{url_append}")
                     soup = BeautifulSoup(url, 'html.parser')
+
                     with open("Output.txt", "w", encoding='utf-8') as text_file:
                         text_file.write(str(soup))
 
@@ -88,14 +55,15 @@ with open("ASIN.txt", "r") as text_file:
 
                         with open('PrimeSellers.csv', 'a+', newline='', encoding='utf-8') as csvfile:
                             csvWriter = csv.writer(csvfile)
-                            
+                                
                             if csvfile.tell() == 0:
-                                csvWriter.writerow(['Product ASIN', 'Seller Name', 'Prime Status', 'Condition'])
-
+                            # csvWriter.writerow(['Product ASIN', 'Seller Name', 'Prime Status', 'Condition'])
+                                csvWriter.writerow(['ASIN', '# of sellers','Sold by AMZ'])
+                            
                             sName = ''
                             pStatus = ''
                             qCondition = ''
-
+                            
                             for i in textParse.findAll("div", {'class': 'a-row a-spacing-mini olpOffer'}):
                                 name = i.find("h3", {'class': 'a-spacing-none olpSellerName'})
                                 qCondition = i.find("div", {'class':'a-section a-spacing-small'}).get_text().strip()
@@ -112,22 +80,38 @@ with open("ASIN.txt", "r") as text_file:
                                 else:
                                     # print("Not Prime")
                                     pStatus = "Not Prime"
-                                if i.find("i", {'class': 'a-icon a-icon-prime'}) and qCondition == 'New' and name.get_text().strip() not in namelist and name.get_text().strip() != 'paneltown':
+                                if i.find("i", {'class': 'a-icon a-icon-prime'}) and name.get_text().strip() not in namelist and qCondition == 'New' and name.get_text().strip() != 'paneltown':
                                     primecounter += 1
                                     namelist.append(name.get_text().strip())
-
+                            try:
+                                nextbutton  = textParse.find("li", {'class':'a-last'}).a['href']
+                                if nextbutton != "None":
+                                     nextpage = True
+                                     time.sleep(3)
+                                else:
+                                    nextpage = False
+                            except:
+                                success_status = True
                                 #print(f'ASIN: {asin.rstrip()}, Seller: {sName}, Prime Status: {pStatus}, Condition: {qCondition}')
-                                
-                                csvWriter.writerow([asin.rstrip(), sName, pStatus, qCondition])
-
-                                # print('\n')
+                                    
+                                #csvWriter.writerow([asin.rstrip(), sName, pStatus, qCondition])
+                #if there is an error with mechanize then try it all again 
                 except:
-                    Error_status = True      
-            if Error_status == False:
+                    try_counter += 1
+                    print('fail')
+                    time.sleep(5)
+                   
+            if try_counter < 2:
                 if '' in namelist:
                     print('Prime Sellers: ' + str(primecounter) + ' AMZ')
+                    with open('PrimeSellers.csv', 'a+', newline='', encoding='utf-8') as csvfile:
+                        csvWriter = csv.writer(csvfile)
+                        csvWriter.writerow([asin.rstrip(), primecounter, 'AMZ'])
                 else: 
                     print('Prime Sellers: '+ str(primecounter))
+                    with open('PrimeSellers.csv', 'a+', newline='', encoding='utf-8') as csvfile:
+                        csvWriter = csv.writer(csvfile)
+                        csvWriter.writerow([asin.rstrip(), primecounter])
             else:
                 print("Failed to Load URL")          
         else:
